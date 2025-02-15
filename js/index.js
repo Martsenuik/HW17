d0fbc9e8e05de0a7600dd83353ac168f92bb7bd5
Martseniuk Alina <martseniuk.av@gmail.com>
2025-02-15 17:33:35 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,58 @@
+//$ЗАВДАННЯ 1
+//*Створити розмітку з кнопкою та текстовим полем.
+//* За допомогою JavaScript отримати доступ до кнопки 
+//* та текстового поля за їх ідентифікаторами та змінити
+//* текст на кнопці на значення текстового поля.
+
+// let input = document.querySelector('#textInput');
+// let btn = document.querySelector('#btn');
+
+// btn.onclick = function () {
+//     btn.innerText = input.value
+// }
+
+//$ЗАВДАННЯ 2
+//*Створити розмітку з заголовком та зображенням.
+//*За допомогою JavaScript отримати доступ до зображення
+//*та змінити значення атрибута "src" на шлях до іншого
+//*зображення.
+
+// document.getElementById("myImage").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQO4zADgpupNzHBTtAefiu-RyKZrBJCyNlJsw&s";
+
+//$ЗАВДАННЯ 3
+//*Створити розмітку з посиланням та зображенням. За допомогою
+//* JavaScript отримати доступ до посилання та змінити значення
+//*атрибута "href" на нову URL-адресу. Також отримати доступ до
+//*зображення та додати новий атрибут "alt" з описом зображення.
+
+//  document.getElementById("myLink").href = "https://w3schoolsua.github.io/index.html#gsc.tab=0";
+        
+// document.getElementById("myImage").alt = "спрацювало";
+
+//$ЗАВДАННЯ 4
+//*Створити розмітку зі списком елементів. За допомогою JavaScript
+//*отримати доступ до першого елемента списку та змінити його вміст
+//*на новий текст.
+
+document.querySelector("ul li").textContent = "було 1 тепер 0";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
